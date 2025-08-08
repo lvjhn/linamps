8649e1a3f5ea4550682a907cd81b686d5f24fca6
LJ S.A. <lvjhn.d@gmail.com>
2025-08-09 02:02:11 +0800

    general updatee

--- utils/fix-permissions.sh
+++ utils/fix-permissions.sh
@@ -1,3 +1,5 @@
 sudo chown -R $USER:$USER -R .
 sudo chmod 755 -R .
 
+sudo chmod -R 775 source/backend/storage source/backend/bootstrap/cache
+sudo chown -R $USER:$USER source/backend/storage source/backend/bootstrap/cache
